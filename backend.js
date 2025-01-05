1318064d533a7e639767e5aaaa1dc710666b32e6
mehak-guptaa <parthgupta75715@gmail.com>
2025-01-05 21:49:06 +0530

    SneakIt

--- backend.js
+++ backend.js
@@ -4,3 +4,21 @@ user.addEventListener("click", () => {
     (alert("Directing to Sign Up Page."))
     window.open('user.html')
 })
+
+let cart = document.getElementById("cart");
+user.addEventListener("click", () => {
+    (alert("Directing to the Cart."))
+    window.open('cart.html')
+})
+
+
+let email = document.getElementById("email");
+
+let signup = document.getElementById("signup");
+signup.addEventListener("click", async () => {
+    try{
+            let res = await email.value
+            alert("Congratulations! Sign Up Successful.")
+        }
+        catch(err){alert("Error Occured. Please try again in sometime.")}
+})
